52562ccde9b64ecb7d74abdfb3ad5233e04296cc
GavalaN <gavalan@kkszki.hu>
2025-01-06 21:48:21 +0100

    homepage search frame

--- frontend/src/Components/HomeSearch.js
+++ frontend/src/Components/HomeSearch.js
@@ -4,32 +4,38 @@ import './HomeSearch.css'
 export default function HomeSearch() {
   return (
     <div id='home-search'>
-        <form id='home-search-form'>
-            <div className='row'>
-              <div className='col-2'>
-                <label for='manufacturer' className='align-self-start'>Márka</label><br/>
+        <form id='home-search-form' className=''>
+            <div className='row d-flex'>
+              <div className='col-auto'>
+                <label for='manufacturer' className=''>Márka</label><br/>
                 <input id='manufacturer' name='manufacturer' className='lg-input'></input>
               </div>
-              <div class="col-2">
-                <label for='type' className='align-self-start'>Típus</label><br/>
+              <div class="col-auto">
+                <label for='type' className=''>Típus</label><br/>
                 <input id='type' name='type' className='lg-input'></input>
               </div>
-              <div class="col-2">
-                <label for='fuel' className='align-self-start'>Üzemanyag</label><br/>
+              <div class="col-auto">
+                <label for='fuel' className=''>Üzemanyag</label><br/>
                 <input id='fuel' name='fuel' className='lg-input'></input>
               </div>
-              <div class="col-2">
-                <label for='yearfrom' className='align-self-start'>Évjárat</label><br/>
-                <input id='yearfrom' className='sm-input-f'></input>
-                <input id='yearto' className='sm-input'></input>
+              <div class="col-auto">
+                <label for='yearfrom' className=''>Évjárat</label><br/>
+                <input id='yearfrom' className='sm-input-f' placeholder='-tól'></input>
+                <input id='yearto' className='sm-input' placeholder='-ig'></input>
               </div>
-              <div class="col-2">
-                <label for='pricefrom' className='align-self-start'>Vételár</label><br/>
-                <input id='pricefrom' className='sm-input-f'></input>
-                <input id='priceto' className='sm-input'></input>
+              <div class="col-auto">
+                <label for='pricefrom' className=''>Vételár</label><br/>
+                <input id='pricefrom' className='sm-input-f' placeholder='-tól'></input>
+                <input id='priceto' className='sm-input' placeholder='-ig'></input>
               </div>
-            </div> 
-        </form>  
+            </div>
+            <div id='home-search-lower' className='row'>
+              <div className='col-6'>
+                <p>Részletes keresés v</p>
+                <button className='btn'>Keresés</button>
+              </div>
+            </div>
+        </form>
     </div>
   )
 }
