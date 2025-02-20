358c951a3902179441cf3cc2339caef30281ac8e
TMarcell <tulakm@kkszki.hu>
2025-02-20 08:23:49 +0100

    Added GetColor

---
+++ AA_Backend/Controllers/ColorController.cs
@@ -0,0 +1,35 @@
+﻿using AA_Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AA_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColorController : ControllerBase
+    {
+        [HttpGet("GetColor")]
+        public IActionResult GetColor()
+        {
+            using (var context = new CarplaceContext())
+            {
+                try
+                {
+                    List<Color> list = context.Colors.ToList();
+                    return Ok(list);
+                }
+                catch (Exception ex)
+                {
+                    List<Color> list = new List<Color>();
+                    Color color = new()
+                    {
+                        Id = -1,
+                        Name = ex.Message
+                    };
+                    list.Add(color);
+                    return BadRequest(list);
+                }
+            }
+        }
+    }
+}
--- AA_Backend/obj/Release/net8.0/AA_Backend.AssemblyInfo.cs
+++ AA_Backend/obj/Release/net8.0/AA_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AA_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fc051ba25e6c596dda848f3bf64644f4e4299cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d38dd84f0660896691be03bc290c22b9812ef10")]
 [assembly: System.Reflection.AssemblyProductAttribute("AA_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AA_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
