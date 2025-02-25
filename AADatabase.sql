e88ff41fa6e127057b1f6e7f8f508ce6a96e9028
TMarcell <tulakm@kkszki.hu>
2025-02-25 10:15:04 +0100

    Database update

--- AADatabase.sql
+++ AADatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2025 at 09:34 AM
+-- Generation Time: Feb 25, 2025 at 10:14 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -66,22 +66,32 @@ CREATE TABLE `cars` (
   `seller_id` int(9) NOT NULL,
   `upload_date` datetime NOT NULL,
   `sold` tinyint(1) NOT NULL,
-  `body_type` varchar(32) NOT NULL
+  `body_type` varchar(32) NOT NULL,
+  `num_of_cyl` int(11) NOT NULL,
+  `horsepower` int(11) NOT NULL,
+  `cc` int(11) NOT NULL,
+  `engine_type` varchar(12) NOT NULL,
+  `fuel_type` varchar(11) NOT NULL,
+  `drive` varchar(50) NOT NULL,
+  `trans_type` varchar(50) NOT NULL,
+  `Year` int(9) NOT NULL,
+  `k_weight` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `cars`
 --
 
-INSERT INTO `cars` (`id`, `brand_id`, `pic_id`, `type_id`, `description`, `km_clock`, `color_id`, `price`, `seller_id`, `upload_date`, `sold`, `body_type`) VALUES
-(1, 1, 1, 1, 'Fuel-efficient and reliable.', 40000, 3, 18000, 1, '2024-01-10 10:00:00', 0, 'Sedan'),
-(2, 2, 1, 2, 'Comfortable and spacious.', 25000, 2, 25000, 1, '2024-02-05 14:30:00', 0, 'Sedan'),
-(3, 3, 1, 3, 'Strong and powerful SUV.', 60000, 5, 32000, 1, '2023-12-01 09:45:00', 1, 'SUV'),
-(4, 4, 1, 4, 'Eladó sorba került BMW 320i autóm!\r\nAmit tudni kell róla:\r\n2006 év\r\nPopsnBangs\r\nSzakadás mentes beltér.\r\nM lökhárítók, M felni, M kormány.\r\nAngel Eyes.\r\nM3 visszapillantók.\r\nMűködő Klíma.\r\nVétel ár: 1,800,000ft\r\nCsere érdekelhet, kisebb-nagyobb \r\nautóra!', 20000, 1, 42000, 1, '2024-03-15 16:20:00', 0, 'Coupe'),
-(5, 5, 1, 5, 'Luxury and performance.', 18000, 4, 48000, 1, '2023-11-20 08:10:00', 1, 'Sedan'),
-(6, 6, 1, 6, 'Large family-friendly SUV.', 55000, 6, 35000, 1, '2024-01-25 12:50:00', 0, 'SUV'),
-(7, 7, 1, 7, 'Efficient and compact.', 30000, 7, 19000, 1, '2024-02-20 11:15:00', 0, 'Sedan'),
-(8, 8, 1, 8, 'Reliable mid-size sedan.', 75000, 8, 22000, 1, '2023-10-10 07:35:00', 1, 'Sedan');
+INSERT INTO `cars` (`id`, `brand_id`, `pic_id`, `type_id`, `description`, `km_clock`, `color_id`, `price`, `seller_id`, `upload_date`, `sold`, `body_type`, `num_of_cyl`, `horsepower`, `cc`, `engine_type`, `fuel_type`, `drive`, `trans_type`, `Year`, `k_weight`) VALUES
+(1, 1, 1, 1, 'Fuel-efficient and reliable.', 40000, 3, 18000, 1, '2024-01-10 10:00:00', 0, 'Sedan', 4, 139, 1798, 'Inline', 'Petrol', 'FWD', 'Automatic', 2022, 1400),
+(2, 2, 1, 2, 'Comfortable and spacious.', 25000, 2, 25000, 1, '2024-02-05 14:30:00', 0, 'Sedan', 4, 192, 1996, 'Inline', 'Petrol', 'FWD', 'CVT', 2021, 1500),
+(3, 3, 1, 3, 'Strong and powerful SUV.', 60000, 5, 32000, 1, '2023-12-01 09:45:00', 1, 'SUV', 6, 400, 3000, 'V', 'Petrol', 'AWD', 'Automatic', 2023, 2100),
+(4, 4, 1, 4, 'Eladó sorba került BMW 320i autóm!\r\nAmit tudni kell róla:\r\n2006 év\r\nPopsnBangs\r\nSzakadás mentes beltér.\r\nM lökhárítók, M felni, M kormány.\r\nAngel Eyes.\r\nM3 visszapillantók.\r\nMűködő Klíma.\r\nVétel ár: 1,800,000ft\r\nCsere érdekelhet, kisebb-nagyobb \r\nautóra!', 20000, 1, 42000, 1, '2024-03-15 16:20:00', 0, 'Coupe', 6, 382, 2998, 'Inline', 'Petrol', 'RWD', 'Automatic', 2022, 1700),
+(5, 5, 1, 5, 'Luxury and performance.', 18000, 4, 48000, 1, '2023-11-20 08:10:00', 1, 'Sedan', 4, 255, 1991, 'Inline', 'Petrol', 'RWD', 'Automatic', 2023, 1800),
+(6, 6, 1, 6, 'Large family-friendly SUV.', 55000, 6, 35000, 1, '2024-01-25 12:50:00', 0, 'SUV', 8, 420, 5300, 'V', 'Petrol', 'AWD', 'Automatic', 2021, 2500),
+(7, 7, 1, 7, 'Efficient and compact.', 30000, 7, 19000, 1, '2024-02-20 11:15:00', 0, 'Sedan', 4, 149, 1997, 'Inline', 'Petrol', 'FWD', 'CVT', 2020, 1300),
+(8, 8, 1, 8, 'Reliable mid-size sedan.', 75000, 8, 22000, 1, '2023-10-10 07:35:00', 1, 'Sedan', 4, 174, 1984, 'Inline', 'Petrol', 'FWD', 'Manual', 2019, 1400),
+(13, 1, 1, 1, 'A nice car', 50000, 1, 15000, 1, '2025-02-24 11:43:38', 0, 'Sedan', 4, 150, 2000, 'Inline', 'Petrol', 'FWD', 'Automatic', 2020, 1500);
 
 -- --------------------------------------------------------
 
@@ -112,35 +122,6 @@ INSERT INTO `colors` (`id`, `name`, `hexcode`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `motors`
---
-
-CREATE TABLE `motors` (
-  `id` int(9) NOT NULL,
-  `num_of_cyl` int(11) NOT NULL,
-  `horsepower` int(11) NOT NULL,
-  `cc` int(11) NOT NULL,
-  `engine_type` varchar(12) NOT NULL,
-  `fuel_type` varchar(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `motors`
---
-
-INSERT INTO `motors` (`id`, `num_of_cyl`, `horsepower`, `cc`, `engine_type`, `fuel_type`) VALUES
-(1, 4, 139, 1798, 'Inline', 'Petrol'),
-(2, 4, 192, 1996, 'Inline', 'Petrol'),
-(3, 6, 400, 3000, 'V', 'Petrol'),
-(4, 6, 382, 2998, 'Inline', 'Petrol'),
-(5, 4, 255, 1991, 'Inline', 'Petrol'),
-(6, 8, 420, 5300, 'V', 'Petrol'),
-(7, 4, 149, 1997, 'Inline', 'Petrol'),
-(8, 4, 174, 1984, 'Inline', 'Petrol');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `pictues`
 --
 
@@ -166,27 +147,22 @@ INSERT INTO `pictues` (`id`, `car_id`, `filePath`) VALUES
 CREATE TABLE `types` (
   `id` int(9) NOT NULL,
   `brand_id` int(9) NOT NULL,
-  `type_name` varchar(255) NOT NULL,
-  `motor_id` int(9) NOT NULL,
-  `drive` varchar(50) NOT NULL,
-  `trans_type` varchar(50) NOT NULL,
-  `Year` int(9) NOT NULL,
-  `k_weight` int(11) NOT NULL
+  `type_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `types`
 --
 
-INSERT INTO `types` (`id`, `brand_id`, `type_name`, `motor_id`, `drive`, `trans_type`, `Year`, `k_weight`) VALUES
-(1, 1, 'Toyota Corolla', 1, 'FWD', 'Automatic', 2022, 1400),
-(2, 2, 'Honda Accord', 2, 'FWD', 'CVT', 2021, 1500),
-(3, 3, 'Ford Explorer', 3, 'AWD', 'Automatic', 2023, 2100),
-(4, 4, 'BMW 3 Series', 4, 'RWD', 'Automatic', 2022, 1700),
-(5, 5, 'Mercedes C-Class', 5, 'RWD', 'Automatic', 2023, 1800),
-(6, 6, 'Chevrolet Tahoe', 6, 'AWD', 'Automatic', 2021, 2500),
-(7, 7, 'Nissan Sentra', 7, 'FWD', 'CVT', 2020, 1300),
-(8, 8, 'Volkswagen Passat', 8, 'FWD', 'Manual', 2019, 1400);
+INSERT INTO `types` (`id`, `brand_id`, `type_name`) VALUES
+(1, 1, 'Toyota Corolla'),
+(2, 2, 'Honda Accord'),
+(3, 3, 'Ford Explorer'),
+(4, 4, 'BMW 3 Series'),
+(5, 5, 'Mercedes C-Class'),
+(6, 6, 'Chevrolet Tahoe'),
+(7, 7, 'Nissan Sentra'),
+(8, 8, 'Volkswagen Passat');
 
 -- --------------------------------------------------------
 
@@ -213,7 +189,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `username`, `email`, `phone_num`, `HASH`, `created`, `is_admin`, `SALT`, `IsActive`) VALUES
 (1, 'admin', 'tulakm@kkszki.hu', '123123123', 'admin', '2025-02-04 11:05:38', 1, 'ads', 0),
 (2, 'string', 'string', 'string', '473287f8298dba7163a897908958f7c0eae733e25d2e027992ea2edc9bed2fa8', '2025-02-18 12:25:08', 0, 'string', 0),
-(3, 'string1', 'budahazim@kkszki.hu', 'string', '473287f8298dba7163a897908958f7c0eae733e25d2e027992ea2edc9bed2fa8', '2025-02-18 13:10:14', 0, 'string', 0);
+(3, 'string1', 'budahazim@kkszki.hu', 'string', '473287f8298dba7163a897908958f7c0eae733e25d2e027992ea2edc9bed2fa8', '2025-02-18 13:10:14', 0, 'string', 0),
+(5, 'string3', 'gavalan@kkszki.hu', 'string', '473287f8298dba7163a897908958f7c0eae733e25d2e027992ea2edc9bed2fa8', '2025-02-21 08:06:03', 0, 'string', 0);
 
 --
 -- Indexes for dumped tables
@@ -243,12 +220,6 @@ ALTER TABLE `colors`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `motors`
---
-ALTER TABLE `motors`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `pictues`
 --
 ALTER TABLE `pictues`
@@ -260,8 +231,7 @@ ALTER TABLE `pictues`
 --
 ALTER TABLE `types`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `brand_id` (`brand_id`),
-  ADD KEY `motor_id` (`motor_id`);
+  ADD KEY `brand_id` (`brand_id`);
 
 --
 -- Indexes for table `users`
@@ -283,18 +253,12 @@ ALTER TABLE `brands`
 -- AUTO_INCREMENT for table `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `colors`
 --
 ALTER TABLE `colors`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `motors`
---
-ALTER TABLE `motors`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
@@ -313,7 +277,7 @@ ALTER TABLE `types`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -333,8 +297,7 @@ ALTER TABLE `cars`
 -- Constraints for table `types`
 --
 ALTER TABLE `types`
-  ADD CONSTRAINT `types_ibfk_1` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`),
-  ADD CONSTRAINT `types_ibfk_2` FOREIGN KEY (`motor_id`) REFERENCES `motors` (`id`);
+  ADD CONSTRAINT `types_ibfk_1` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
