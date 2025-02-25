49da88c4471240b4cb61701d8c25aa58ba44a33c
TMarcell <tulakm@kkszki.hu>
2025-02-25 10:33:25 +0100

    Added missing JsonIgnores

--- AA_Backend/Models/User.cs
+++ AA_Backend/Models/User.cs
@@ -16,13 +16,13 @@ public partial class User
 
     public string Hash { get; set; } = null!;
 
-    public DateTime Created { get; set; }
+    public DateTime? Created { get; set; }
 
-    public bool IsAdmin { get; set; }
+    public bool? IsAdmin { get; set; }
 
     public string Salt { get; set; } = null!;
 
-    public int IsActive { get; set; }
+    public int? IsActive { get; set; }
     [JsonIgnore]
     public virtual ICollection<Car> Cars { get; set; } = new List<Car>();
 }
--- AA_Backend/obj/Debug/net8.0/AA_Backend.AssemblyInfo.cs
+++ AA_Backend/obj/Debug/net8.0/AA_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AA_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce7131061d34c33332330c8de67dfefbd8526008")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4cc540c2265d5b27f2970864728495ea6b47778")]
 [assembly: System.Reflection.AssemblyProductAttribute("AA_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AA_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
