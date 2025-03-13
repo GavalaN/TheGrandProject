42a7a3cbaa97e5e43b0669f9b5ae22ddcbdc9cd6
TMarcell <tulakm@kkszki.hu>
2025-03-13 07:36:46 +0100

    Loose files

--- AA_Backend/obj/Debug/net8.0/AA_Backend.AssemblyInfo.cs
+++ AA_Backend/obj/Debug/net8.0/AA_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AA_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79cf27ba70893a2217a4a7c1e61fd5002106d515")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+075f36b3a6282f2eb252b8eef35870d3faeb5780")]
 [assembly: System.Reflection.AssemblyProductAttribute("AA_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AA_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
