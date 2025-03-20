5d8c6ac56f776dbfb0711efcc90cca53f948cade
Gavalan <gavalan@kkszki.hu>
2025-03-20 11:04:02 +0100

    0.4.3.1v documents added

--- frontend/src/Pages/Documents.js
+++ frontend/src/Pages/Documents.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import adasveteli from '../Documents/Gepjarmu_adas-veteli_szerzodesminta.pdf'
+import kgfb from '../Documents/Kotelezo_gepjarmu_felelosegbiztositas.pdf'
+import torzskonyv from '../Documents/Torzskonyv_forgalmi_engedely.pdf'
 import { Link } from 'react-router-dom'
 
 export default function Documents() {
@@ -8,6 +10,12 @@ export default function Documents() {
         <h2 className="text-center">Dokumentumtár</h2>
         <h4>Adásvételi szerződés</h4>
         <p>Az adásvételi szerződés mintát megtekintheti és letöltheti <Link to={adasveteli} target='_blank'>Itt</Link>.</p>
+        <hr/>
+        <h4>Kötelező gépjármű felelőségbiztosítás</h4>
+        <p>A kötelező gépjármű felelőségbiztosítással kapcsolatos törvényt megtekintheti és letöltheti <Link to={kgfb} target='_blank'>Itt</Link>.</p>
+        <hr/>
+        <h4>Törzskönyv és forgalmi engedély</h4>
+        <p>A Törzskönyvvel és forgalmi engedéllyel kapcsolatos törvényt megtekintheti és letöltheti <Link to={torzskonyv} target='_blank'>Itt</Link>.</p>
         <hr/>
     </div>
   )
