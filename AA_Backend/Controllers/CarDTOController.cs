05f556ccf887f57b152fc0af0a36976c70d3aa3d
TMarcell <tulakm@kkszki.hu>
2025-03-27 07:48:29 +0100

    Added GetPage

--- AA_Backend/Controllers/CarDTOController.cs
+++ AA_Backend/Controllers/CarDTOController.cs
@@ -7,10 +7,10 @@ namespace AA_Backend.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    
+
     public class CarDTOController : ControllerBase
     {
-        
+
 
         [HttpGet("GetAll")]
         public IActionResult GetDTOAll()
@@ -49,6 +49,43 @@ namespace AA_Backend.Controllers
                     };
                     list.Add(car);
                     return BadRequest(list);
+                }
+            }
+        }
+        [HttpGet("GetPage")]
+        public IActionResult GetDTOPage(int page,int pagesize)
+        {
+            using (var context = new CarplaceContext())
+            {
+                try
+                {
+                    List<CarDTO> CarDTOs = context.Cars.Select(k => new CarDTO()
+                    {
+                        Id = k.Id,
+                        SellerId = k.SellerId,
+                        Brand = k.Brand.Name,
+                        Type_name = k.Type.TypeName,
+                        KMClock = k.KmClock,
+                        Price = k.Price,
+                        Description = k.Description,
+                        ccm = k.Cc,
+                        hp = k.Horsepower,
+                        fuel_type = k.FuelType,
+                        Year = k.Year
+                    }).Skip((page - 1) * pagesize).Take(pagesize).ToList();
+                    return Ok(CarDTOs);
+                }
+                catch (Exception ex)
+                {
+                    List<CarDTO> list = new List<CarDTO>();
+                    CarDTO car = new()
+                    {
+                        Id = 1,
+                        Description = ex.Message
+                    };
+                    list.Add(car);
+                    return BadRequest(list);
+
                 }
             }
         }
--- AA_Backend/obj/Debug/net8.0/AA_Backend.AssemblyInfo.cs
+++ AA_Backend/obj/Debug/net8.0/AA_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AA_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13993650fa455b207ec31ef8e800bdee707959d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01263ba1eeaf6ebc7d3dda944a88fea7cf70c10b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AA_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AA_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
