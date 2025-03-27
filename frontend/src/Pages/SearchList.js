202c47882670ef87467439c8f786083fc1ca56df
GavalaN <gavalan@kkszki.hu>
2025-03-27 08:42:38 +0100

    0.4.5.1v pagination implemented, not working

--- frontend/src/Pages/SearchList.js
+++ frontend/src/Pages/SearchList.js
@@ -3,10 +3,13 @@ import SearchSide from '../Components/SearchSide'
 import axios from 'axios'
 import CarCard from '../Components/CarCard'
 import './SearchList.css'
+import ReactPaginate from 'react-paginate'
 
 export default function SearchList() {
   const base_url = process.env.REACT_APP_BASE_URL;
   const [cars, setCars] = useState([])
+  const [pageSize, setPageSize] = useState(10);
+  const [page, setPage] = useState(1)
 
   // useEffect(() => {
   //   axios.get(base_url+'/CarDTO/GetAll')
@@ -30,6 +33,16 @@ export default function SearchList() {
           {cars.map(car => {
             return <CarCard id={car.id} brand={car.brand} type_name={car.type_name} fuel_type={car.fuel_type} year={car.year} ccm={car.ccm} horsepower={car.hp} odometer={car.kmClock} price={car.price} description={car.description}/>
           })}
+          <ReactPaginate
+            containerClassName={"pagination d-flex justify-content-center"}
+            breakLabel="..."
+            nextLabel="előző >"
+            onPageChange={contentChange}
+            pageRangeDisplayed={pageSize}
+            pageCount={page}
+            previousLabel="< következő"
+            renderOnZeroPageCount={null}
+          />
         </div>
       </div>
     </div>
